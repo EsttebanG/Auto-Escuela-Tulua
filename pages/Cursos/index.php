77b98b1fa10db85d3a0772996131702c79fd056e
EsttebanG <ebgaray9@misena.edu.co>
2024-05-27 09:34:28 -0500

    .

--- pages/Cursos/index.php
+++ pages/Cursos/index.php
@@ -2,11 +2,7 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/Auto-Escuela-Tulua/img/a.png" type="image/png">
-    <!-- Icono de la página -->
-    <link rel="stylesheet" href="/Auto-Escuela-Tulua/assets/css/style.css">
+    <?php include(../../aseets/config/head.php);?>
     <title>Auto Escuela Tuluá - Cursos</title>
 </head>
 <body>
