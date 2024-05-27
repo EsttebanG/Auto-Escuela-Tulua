b2777cebeacd6c2b51d8475a1521a3a4c4ee425a
EsttebanG <ebgaray9@misena.edu.co>
2024-05-27 10:44:12 -0500

    .

--- pages/Pagos/Curso_1/index.php
+++ pages/Pagos/Curso_1/index.php
@@ -42,27 +42,11 @@
         </div>
         </div>
     </section>
-    <footer>
-        <div class="footer-container">
-            <div class="footer-section">
-                <h3>Nuestro Respaldo</h3>
-                <img id="Iconte" src="/Auto-Escuela-Tulua/img/Iconte.png" alt="Iconte">
-                <img id="VigiladoSuperTransporte" src="/Auto-Escuela-Tulua/img/VigiladoSuperTransporte.png" alt="VigiladoSuperTransporte">
-                <img id="Transporte" src="/Auto-Escuela-Tulua/img/Transporte.png" alt="Transporte">
-            </div>
-            <div class="footer-section">
-                <p>Horario de admisiones</p>
-                <p>Lunes a viernes de 8:00 am a 7:00 pm</p>
-                <p>Sábados 8:00 am a 5:00 pm</p>
-            </div>
-        </div>
-        <div>
-            <p>AUTO ESCUELA centro de enseñanza automovilística Sede Calle 63: Calle 63 24-28</p>
-            <p>Sede Cedritos: Autopista Norte 146-48 Local 253 Secretaria Educación Resolución 12-0348</p>
-            <p>Mintrasporte Resolución 006436 | Vigilados Superintendencia</p>
-            <p>Teléfonos: Sede calle 63: (601) 9144499 Sede Cedritos: (601) 9144499</p>
-        </div>
-    </footer>
+        <!-- Botón de WhatsApp -->
+        <?php include("../../assets/config/whatsapp.php");?>
+        <!-- Botón de WhatsApp fin -->
+
+        <?php include("../../assets/config/footer.php");?>
 </body>
 
 </html>
