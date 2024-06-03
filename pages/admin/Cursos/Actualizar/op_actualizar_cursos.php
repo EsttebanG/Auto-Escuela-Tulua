4aea46066b8331d33411400694d1d888e9c86966
GeronimoGc <geronimo24gonzalez@gmail.com>
2024-06-03 11:11:33 -0500

    .

--- pages/admin/Cursos/Actualizar/op_actualizar_cursos.php
+++ pages/admin/Cursos/Actualizar/op_actualizar_cursos.php
@@ -7,15 +7,10 @@ $descripcion = $_POST['descripcion'];
 $precio = $_POST['precio'];
 $duracion = $_POST['duracion'];
 $cupos_disponibles = $_POST['cupos_disponibles'];
-$fileImage1 = $_FILES['fileImage']['name'];
-$tmpFileImage = $_FILES['fileImage']['tmp_name'];
-$ruteFile = '../../../../img/Curso/'.$fileImage1;
 
-move_uploaded_file($tmpFileImage,$ruteFile);
 
-$stmt = $pdo->prepare('UPDATE curso SET nombre = ?, descripcion = ?, curso_img_name = ?, precio = ?, duracion = ?, cupos_disponibles = ? WHERE id_curso = ?');
-$stmt->execute([$nombre, $descripcion,  $fileImage1, $precio, $duracion, $cupos_disponibles, $id]);
+$stmt = $pdo->prepare('UPDATE curso SET nombre = ?, descripcion = ?, precio = ?, duracion = ?, cupos_disponibles = ? WHERE id_curso = ?');
+$stmt->execute([$nombre, $descripcion, $precio, $duracion, $cupos_disponibles, $id]);
 
 header('Location: ../');
 exit;
-?>
--- pages/admin/Cursos/Crear/op_crear_cursos.php
+++ pages/admin/Cursos/Crear/op_crear_cursos.php
@@ -1,21 +1,21 @@
-<?php
-require '../../../../assets/config/db.php';
+    <?php
+    require '../../../../assets/config/db.php';
 
-$nombre = $_POST['nombre'];
-$descripcion = $_POST['descripcion'];
-$precio = $_POST['precio'];
-$duracion = $_POST['duracion'];
-$cupos_disponibles = $_POST['cupos_disponibles'];
-$fileImage1 = $_FILES['fileImage']['name'];
-$tmpFileImage = $_FILES['fileImage']['tmp_name'];
-$ruteFile = '../../../../img/Curso/'.$fileImage1;
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+    $precio = $_POST['precio'];
+    $duracion = $_POST['duracion'];
+    $cupos_disponibles = $_POST['cupos_disponibles'];
+    $fileImage1 = $_FILES['fileImage']['name'];
+    $tmpFileImage = $_FILES['fileImage']['tmp_name'];
+    $ruteFile = '../../../../img/Curso/' . $fileImage1;
 
-move_uploaded_file($tmpFileImage,$ruteFile);
+    move_uploaded_file($tmpFileImage, $ruteFile);
 
-$stmt = $pdo->prepare('INSERT INTO curso (nombre, descripcion, curso_img_name, precio, duracion, cupos_disponibles) VALUES (?, ?, ?, ?, ?, ?)');
-$stmt->execute([$nombre, $descripcion, $fileImage1, $precio, $duracion, $cupos_disponibles]);
+    $stmt = $pdo->prepare('INSERT INTO curso (nombre, descripcion, curso_img_name, precio, duracion, cupos_disponibles) VALUES (?, ?, ?, ?, ?, ?)');
+    $stmt->execute([$nombre, $descripcion, $fileImage1, $precio, $duracion, $cupos_disponibles]);
 
 
-header('Location:../');
-exit;
-?>
+    header('Location:../');
+    exit;
+    ?>
